1568b78ce2579d8e895cedcf2e846e9c926b6e13
fahrezyariel84@gmail.com <lukmanulhakim11jun@gmail.com>
2023-09-14 10:53:21 +0700

    TESS

--- routes/web.php
+++ routes/web.php
@@ -22,24 +22,19 @@ Route::get('/',[DashboardController::class,'index']);
 
 Route::get('/daftar-barang', [BarangController::class, 'index'])->name('barang');
 Route::post('/daftar-barang/baru', [BarangController::class, 'tambah']);
-// Route::get('/daftar-barang/edit/{barang}', [BarangController::class, 'edit']);
 Route::post('/daftar-barang/edit', [BarangController::class, 'editBarang']);
-// Route::get('/daftar-barang/hapus/{barang}', [BarangController::class, 'hapus']);
 Route::post('/daftar-barang/hapus', [BarangController::class, 'hapusBarang']);
 Route::get('/daftar-barang/restore/{product_id}', [BarangController::class, 'restore']);
 Route::get('/daftar-barang/deleted', [BarangController::class, 'deleted']);
 
 Route::get('/kategori-barang', [KategoriController::class, 'index']);
 Route::post('/kategori-barang/tambah', [KategoriController::class, 'tambah']);
-// Route::get('/kategori-barang/edit/{category}', [KategoriController::class, 'edit']);
 Route::post('/kategori-barang/edit', [KategoriController::class, 'editJadi']);
-// Route::get('/kategori-barang/hapus/{category}', [KategoriController::class, 'hapus']);
 Route::post('/kategori-barang/hapus', [KategoriController::class, 'hapusJadi']);
 Route::get('/kategori-barang/restore/{category_id}', [KategoriController::class, 'restore']);
 Route::get('/kategori-barang/deleted', [KategoriController::class, 'deleted']);
 
 Route::get('/data-pegawai', [PegawaiController::class, 'index']);
-// Route::get('/data-pegawai/hapus/{user}', [PegawaiController::class, 'hapus']);
 Route::post('/data-pegawai/detail', [PegawaiController::class, 'detailJadi']);
 Route::post('/data-pegawai/hapus', [PegawaiController::class, 'hapusJadi']);
 Route::get('/data-pegawai/restore/{user_id}', [PegawaiController::class, 'restore']);
